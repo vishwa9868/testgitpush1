50a82de2c890d70a2f8c4256bce36244ba2867d9
LENOVO <vishwanathpatil023@gmail.com>
2022-08-06 23:59:53 +0530

    this is my first code push in git hub

--- mongodb1.py
+++ mongodb1.py
@@ -1,9 +1,10 @@
+
 import pymongo
 client = pymongo.MongoClient("mongodb+srv://vish9868:Patilvish9868@cluster0.mlebtjg.mongodb.net/?retryWrites=true&w=majority")
 db = client.test
 print(db)
 
-d = {
+{
     "name": "vishwa",
     "email": "vishwanathpatil023@gmail.com",
     "surname": "patil"
@@ -11,3 +12,15 @@ d = {
 db1 = client['pythonProject1']
 coll = db1['test']
 coll.insert_one(d)
+
+{
+    "name": "vishwa",
+    "email": "vishwanathpatil023@gmail.com",
+    "surname": "patil"
+}
+
+{
+    "name": "vishwa",
+    "email": "vishwanathpatil023@gmail.com",
+    "surname": "patil"
+}
